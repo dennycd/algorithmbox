2547407472bb80def11b82bd8b81ccc2e489ae81
Denny C. Dai <dennycd@me.com>
2013-06-19 16:38:57 -0700

    npm package info update

---
+++ index.js
@@ -0,0 +1,31 @@
+//base class for optimization problem and solution
+module.exports.OProblem = require('./src/core/OProblem.js');
+module.exports.OSolution = require('./src/core/OSolution.js');
+
+//base class for stochasic local search algorithm
+module.exports.SLS = require('./src/core/SLS.js');
+
+//3 major SLS algorithm
+module.exports.IIA = require('./src/core/IIA.js');
+module.exports.SA = require('./src/core/SA.js');
+module.exports.TS = require('./src/core/TS.js');
+
+//experiment support
+module.exports.Experiment = require('./src/core/Experiment.js').Experiment;
+module.exports.Analyzer = require('./src/core/Experiment.js').Analyzer;
+
+
+module.exports.Example = {};
+
+//example SAT Problem
+Example.SAT = require('./src/example/sat/Problem.js');
+Example.GSAT = require('./src/example/sat/Algorithms.js').GSAT_IIA;
+Example.SAT_SA = require('./src/example/sat/Algorithms.js').SAT_SA;
+Example.SAT_TS = require('./src/example/sat/Algorithms.js').SAT_TS;
+
+//example TSP Problem
+Example.TSP = require('./src/example/tsp/Problem.js');
+Example.TSP_Solution = require('./src/example/tsp/Solution.js');
+Example.IIA = require('./src/example/tsp/Algorithms.js').TSP_IIA;
+Example.SA = require('./src/example/tsp/Algorithms.js').TSP_SA;
+Example.TS = require('./src/example/tsp/Algorithms.js').TSP_TS;
